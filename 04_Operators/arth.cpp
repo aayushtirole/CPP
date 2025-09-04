62631070006a2fc5e041cc1fa38749e351c563cf
aayushtirole <aayushtirole22@gmail.com>
2025-09-04 21:58:05 +0530

    .

---
+++ 04_Operators/arth.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int cups;
+    double pricePerCup, totalPrice, discountPrice;
+    
+    cout << "Enter the number of tea cups:";
+    cin >> cups;
+    cout << "Enter the price per cups:";
+    cin >> pricePerCup;
+    
+    totalPrice = cups * pricePerCup;
+    
+    //Apply 5% discount if total price is above 100
+    
+    if(totalPrice > 100){
+        discountPrice = totalPrice - (totalPrice * 0.05);
+        cout << "Discount price is:" << discountPrice << endl;
+    } else {
+        cout << "Total price is " << totalPrice << endl;
+    }
+}
