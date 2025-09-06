0d7be941846ce574c72d103c7a13d3d3a47eb35d
aayushtirole <aayushtirole22@gmail.com>
2025-09-06 17:04:12 +0530

    arr

---
+++ 08_Array/basic2/point.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Pointer arithematic 
+// address new = address current + i * size of data type 
+
+int main (){
+    int marks[4] = {23,45,56,89};
+
+    int* p = marks;
+
+    cout<<"The value of marks[0] is" << *p << endl;
+    cout<<"The value of marks[1] is" << *(p+1) << endl;
+    cout<<"The value of marks[2] is" << *(p+2 )<< endl;
+    cout<<"The value of marks[3] is" << *(p+3 )<< endl;
+    
+    return 0;
+}
