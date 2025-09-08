6c08503ab89dece4b52be07e14b7cf2859e4468a
aayushtirole <aayushtirole22@gmail.com>
2025-09-09 00:31:03 +0530

    indexing

---
+++ 09_string/indexing.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s ="aayush tirole";
+    // cout <<s;
+    cout << s << endl;
+    s[0] = 'M';  //update
+    s[2] = 'e'; // update
+    cout << s << endl;
+return 0;
+}
