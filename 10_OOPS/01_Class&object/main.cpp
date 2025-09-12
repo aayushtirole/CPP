1bd46032b221872870fd42b84f1658b0bd72a01f
aayushtirole <aayushtirole22@gmail.com>
2025-09-12 05:46:14 +0530

    class and object initial

---
+++ 10_OOPS/01_Class&object/main.cpp
@@ -0,0 +1,20 @@
+// Class and object 
+
+#include<iostream>
+using namespace std;
+
+class hero{
+
+    // properties 
+    int health;
+   
+};
+
+int main(){
+
+    // creation of object 
+    hero h1;
+    cout<<"size :"<< size of(h1) << endl;
+    
+    return 0;
+}
