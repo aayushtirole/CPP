ec228f6029bd9e9b7fd2bdc712ac50f82fc2bcad
aayushtirole <aayushtirole22@gmail.com>
2025-09-16 08:25:39 +0530

    vec functoion

--- 13_STL/01_Vectors/Vector.cpp
+++ 13_STL/01_Vectors/Vector.cpp
@@ -6,8 +6,10 @@ int main(){
     // Initialization of vector
     // vector<int> vec(3,2);
 
-    vector<int> vec1 = {1, 2, 3, 4, 5, 6};
-    vector<int> vec2(vec1);
+    // vector<int> vec1 = {1, 2, 3, 4, 5, 6};
+    // vector<int> vec2(vec1);
+
+    vector<int> vec = {1, 2, 3, 4, 5, 6};
 
     // vec.push_back(1);
     // vec.push_back(2);
@@ -17,13 +19,25 @@ int main(){
     // vec.emplace_back(6);
 
     // vec.pop_back(); //remove the last element 
+
+    // vec.erase(vec.begin() );
+    // vec.erase(vec.begin() + 2 );
+    // vec.erase(vec.begin()+1, vec.begin() + 3 ); //for delete the range itn the vector 
+
+    // vec.insert(vec.begin()+2, 100);
+
+    vec.clear(); // for clearing the full vector 
+
+    
    
 
-    for (int val:vec2){
-        cout << val <<"";
+    for (int val :vec){
+        cout << val <<" ";
     }
 
     cout << endl;
+
+    cout << "is empty : " << vec.empty() << endl;
 
     // cout << "val at idx 2 : " << vec[2] << " or " << vec.at(2)  << endl; //Print the index element 
 
