b626955c3ce51311907897af0cc394fcd9d0c4ed
aayushtirole <aayushtirole22@gmail.com>
2025-09-16 18:29:48 +0530

    fix

---
+++ 13_STL/01_Vectors/02_iterators/iterators.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+   
+    vector<int> vec = {1, 2, 3, 4, 5, 6};
+
+    cout << "vec.begin : " << *(vec.begin()) << endl;
+
+    cout << "vec.end : " << *(vec.end()) << endl;
+
+    return 0;
+}
