c9d2be0f0010abc172725f584ac0058680d0c5d4
aayushtirole <aayushtirole22@gmail.com>
2025-09-16 08:05:07 +0530

    initialization vector

--- 13_STL/01_Vectors/Vector.cpp
+++ 13_STL/01_Vectors/Vector.cpp
@@ -3,19 +3,23 @@
 using namespace std;
 
 int main(){
-    vector<int> vec;
+    // Initialization of vector
+    // vector<int> vec(3,2);
 
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.push_back(4);
-    vec.push_back(5);
-    vec.emplace_back(6);
+    vector<int> vec1 = {1, 2, 3, 4, 5, 6};
+    vector<int> vec2(vec1);
 
-    vec.pop_back(); //remove the last element 
+    // vec.push_back(1);
+    // vec.push_back(2);
+    // vec.push_back(3);
+    // vec.push_back(4);
+    // vec.push_back(5);
+    // vec.emplace_back(6);
+
+    // vec.pop_back(); //remove the last element 
    
 
-    for (int val:vec){
+    for (int val:vec2){
         cout << val <<"";
     }
 
@@ -23,7 +27,7 @@ int main(){
 
     // cout << "val at idx 2 : " << vec[2] << " or " << vec.at(2)  << endl; //Print the index element 
 
-    cout << "front : " << vec.front()<< endl;
-    cout << "Back : " << vec.back()<< endl;
+    // cout << "front : " << vec.front()<< endl; // remove first element 
+    // cout << "Back : " << vec.back()<< endl;  // remove laste element 
     return 0;
 }
