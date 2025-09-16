17034cec871312516bec28a0c24b20f3786a05a9
aayushtirole <aayushtirole22@gmail.com>
2025-09-16 23:51:24 +0530

    list

--- 13_STL/01_Container/02_list/list.cpp
+++ 13_STL/01_Container/02_list/list.cpp
@@ -1,12 +1,27 @@
 #include<iostream>
 #include<vector>
+#include<list>
 using namespace std;
 
 int main(){
    
-    vector<int> vec = {1, 2, 3, 4, 5, 6};
+    list<int> l;
 
-  
+    l.push_back(1);
+    l.push_back(2);
+    l.push_front(5);
+    l.push_front(4);
+    
+
+    l.pop_back();
+    l.pop_front();
+
+    for (int val : l){
+        cout << val << " ";
+        
+    }
+
+    cout << endl;
 
 
     return 0;
