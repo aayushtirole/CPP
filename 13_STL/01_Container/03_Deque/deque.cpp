40466e9bf37ede751f37dd7ac4117097e3a9a90a
aayushtirole <aayushtirole22@gmail.com>
2025-09-17 00:04:06 +0530

    deque

--- 13_STL/01_Container/03_Deque/deque.cpp
+++ 13_STL/01_Container/03_Deque/deque.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
-#include <vector>
+#include<vector>
+#include<list>
+#include <deque>
 using namespace std;
 
 int main(){
-    
+   
+    deque<int> d = {1, 2, 3, 4, 5};
+
+
+    for (int val : d){
+        cout << val << " ";
+        
+    }
+
+    cout << endl;
+
+    cout << d[2] << endl;
+
     return 0;
 }
