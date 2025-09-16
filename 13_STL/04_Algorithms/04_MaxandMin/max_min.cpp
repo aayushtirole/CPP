bebbaa09e51eeb647e80f9e68fe600e7808bad73
aayushtirole <aayushtirole22@gmail.com>
2025-09-17 05:18:46 +0530

    min max and swap

---
+++ 13_STL/04_Algorithms/04_MaxandMin/max_min.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    
+    cout<<max(4, 5) << " " << min(6, 10) <<endl;
+
+    return 0;
+}
---
+++ 13_STL/04_Algorithms/05_swap/swap.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    
+   int a=5, b=10;
+   swap(a,b);
+   cout<<"a = "<< a <<endl;
+   cout<<"b = "<< b <<endl;
+
+    return 0;
+}
