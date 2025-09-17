af22682de02c5c834c8805c8c713abce73c2158d
aayushtirole <aayushtirole22@gmail.com>
2025-09-17 20:19:06 +0530

    prac1

--- PRACTICE/starregtangle.cpp
+++ PRACTICE/starregtangle.cpp
@@ -1,10 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
-    int m;
-    int n;
-    
+int main() {
+    int rows, cols;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            
+            
+            cout << "* " ;
+        }
+        cout << endl;
+    }
+
     return 0;
 }
