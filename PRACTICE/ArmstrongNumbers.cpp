1ffbfcd6afecc084c624e4b9f985fb844a70da4a
aayushtirole <aayushtirole22@gmail.com>
2025-09-18 20:21:46 +0530

    arm

--- PRACTICE/ArmstrongNumbers.cpp
+++ PRACTICE/ArmstrongNumbers.cpp
@@ -17,12 +17,12 @@ int main(){
         // Example: agar n=153 → pehle 3^3, fir 5^3, fir 1^3 add hoga
     }
 
-    // 🔹 Compare karna: Agar digits ke cube ka sum original number ke barabar hai
+    // 🔹 Compare: Agar digits ke cube ka sum original number ke barabar hai
     if (temp == sum){
         cout << "Number is an Armstrong Number";
     } else {
         cout << "Number is not an Armstrong Number";
     }
 
-    return 0; // 🔹 Program successful end
+    return 0; // 
 }
