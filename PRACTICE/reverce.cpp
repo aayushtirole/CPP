8649897510943e3474812205e8907e57f8480fcc
aayushtirole <aayushtirole22@gmail.com>
2025-09-18 21:13:09 +0530

    re

--- PRACTICE/reverce.cpp
+++ PRACTICE/reverce.cpp
@@ -11,14 +11,14 @@ int main() {
     // Loop jab tak n > 0
     // while (n > 0) {
     //     remainder = n % 10;            // last digit nikaalo
-    //     reverse = reverse * 10 + remainder; // reverse number banate jao
+    //     reverse = (reverse * 10) + remainder; // reverse number banate jao
     //     n = n / 10;                    // ek digit hata do
     // }
 
        // for loop ka use karke reverse nikalna
     for (; n > 0; n = n / 10) {
         remainder = n % 10;                  // last digit nikaalo
-        reverse = reverse * 10 + remainder;  // reverse number banate jao
+        reverse = (reverse * 10) + remainder;  // reverse number banate jao
     }
 
     cout << "The reverse number of " << temp << " is: " << reverse;
