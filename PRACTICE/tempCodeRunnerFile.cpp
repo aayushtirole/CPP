dcb60e2804f99a8297fe62b934d218fc95835a92
aayushtirole22@gmail.com <aayushtirole22@gmail.com>
2025-09-22 12:32:22 +0530

    prac

--- PRACTICE/tempCodeRunnerFile.cpp
+++ PRACTICE/tempCodeRunnerFile.cpp
@@ -1,16 +1,1 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    
-    int n;
-    cout << "Enter the number : " ;
-    cin >> n;
-
-    if (n%2 == 0){
-        cout << "Nummber is a prime";
-    }else{
-        cout << "Nummber is a not prime";
-    }
-    return 0;
-}
+Given marks of a student, print on the screen:
